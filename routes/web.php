47875c733496076db0fe2d939a3368fccc5e9a8c
rezalus7 <zalulaptop@gmail.com>
2022-02-17 13:05:41 +0700

    laravel

---
+++ routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/', function () {
+    return view ('home', [
+        "title" => "Home"
+    ]);
+});
+
+Route::get('/about', function () {
+    return view ('about', [
+        "title" => "About",
+        "nama" => "Rezalu Sihabuddin",
+        "email" => "3103120194@student.smktelkom-pwt.sch.id",
+        "gambar" => "rezalu.png"
+    ]);
+});
+
+Route::get('/gallery', function () {
+    return view ('gallery', [
+        "title" => "Gallery"
+    ]);
+});
+
+Route::get('/contacts', function () {
+    return view ('contacts', [
+        "title" => "Contacts"
+    ]);
+});
+
+Route::resource('/contacts', ContactController::class);
